b2b67b5f215aa1f101e3254f657ae908a61b950b
Reginaldo Morikawa <morikawa77@gmail.com>
2022-10-15 04:24:51 -0300

    First commit

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Envelope } from 'phosphor-react';
+import { TextInput, TextInputRootProps } from './TextInput';
+
+
+export default {
+  title: 'Components/TextInput',
+  component: TextInput.Root,
+  args: {
+    children: [
+      <TextInput.Icon>
+          <Envelope />
+      </TextInput.Icon>,
+      <TextInput.Input placeholder='Type your email address' />
+    ]
+  },
+  argTypes: {}
+} as Meta<TextInputRootProps>
+
+export const Default:StoryObj<TextInputRootProps> ={
+  argTypes: {
+    children: {
+      table: {
+        disable: true,
+      }
+    }
+  }
+}
+
+export const WithoutIcon:StoryObj<TextInputRootProps> ={
+  args: {
+    children: <TextInput.Input placeholder='Type your email address' />
+  }
+}
